6d91b39b0dfe949df039f9546e9d0ee718262614
Teibidh <teibidh@gmail.com>
2019-06-03 16:55:47 -0400

    Initial content

---
+++ src/main/java/io/teibidh/htbpack/htbpack.java
@@ -0,0 +1,31 @@
+package io.teibidh.htbpack;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = htbpack.MODID, name = htbpack.NAME, version = htbpack.VERSION)
+public class htbpack
+{
+    public static final String MODID = "htbpack";
+    public static final String NAME = "Here There Be modpack mod";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("HTB Pack mod loaded!");
+    }
+}
